19d22579ba2b2d81eda39e6c127bd11d1e8a3fdd
tangyun <tangyun@51qub.com>
2021-11-11 11:14:44 +0800

    微信输入姓名电话

--- src/components/Step3.vue
+++ src/components/Step3.vue
@@ -40,39 +40,42 @@
         </div>
       </div>
       <div class="STLiBian tx1">完善持卡人信息</div>
-      <div class="STLiBian inputTips0">
-        请务必填写真实信息以绑定此卡！
-      </div>
-      <div
-        :class="{
-          inputArea: true,
-          styleSelf: deliverType === 'self'
-        }"
-      >
-        <input
-          :value="name"
-          @input="changeInputName"
-          :placeholder="'请填写您的真实姓名'"
-          @blur="inputBlur"
-        />
-        <div class="ok" v-if="validateName"></div>
-      </div>
-      <div class="STLiBian inputTips" v-show="deliverType !== 'self'"></div>
-      <div
-        :class="{
-          inputArea: true,
-          styleSelf: deliverType === 'self'
-        }"
-      >
-        <input
-          :value="mobile"
-          @input="changeInputMobile"
-          :placeholder="'请填写您常用的手机号码'"
-          @blur="inputBlur"
-        />
-        <div class="ok" v-if="validateMobile"></div>
-      </div>
-      <div class="STLiBian inputTips" v-show="deliverType !== 'self'"></div>
+      <template v-if="!wxReady || deliverType === 'self'">
+        <div class="STLiBian inputTips0">
+          请务必填写真实信息以绑定此卡！
+        </div>
+        <div
+          :class="{
+            inputArea: true,
+            styleSelf: deliverType === 'self'
+          }"
+        >
+          <input
+            :value="name"
+            @input="changeInputName"
+            :placeholder="'请填写您的真实姓名'"
+            @blur="inputBlur"
+          />
+          <div class="ok" v-if="validateName"></div>
+        </div>
+        <div class="STLiBian inputTips" v-show="deliverType !== 'self'"></div>
+        <div
+          :class="{
+            inputArea: true,
+            styleSelf: deliverType === 'self'
+          }"
+        >
+          <input
+            :value="mobile"
+            @input="changeInputMobile"
+            :placeholder="'请填写您常用的手机号码'"
+            @blur="inputBlur"
+          />
+          <div class="ok" v-if="validateMobile"></div>
+        </div>
+        <div class="STLiBian inputTips" v-show="deliverType !== 'self'"></div>
+      </template>
+
       <div class="tab" v-show="deliverType === 'express'">
         <div v-if="!wxReady" class="inputArea">
           <input
@@ -83,9 +86,13 @@
           />
           <div class="ok" v-if="validateAddress"></div>
         </div>
-        <div v-else class="shurudizhi" @click="getWxAddressHander">
-          <img :src="require('./images/shurudizi.png')" alt />
-        </div>
+        <template v-else>
+          <div class="shurudizhi" @click="getWxAddressHander">
+            <img :src="require('./images/shurudizi.png')" alt />
+          </div>
+          <div class="STLiBian inputTips padding-bottom-30">请务必填写真实信息以绑定此卡！</div>
+        </template>
+
         <div class="STLiBian inputTips"></div>
       </div>
       <div
@@ -129,113 +136,128 @@
 </template>
 
 <script>
-import { mapState, mapMutations, mapActions } from "vuex";
-import StepsIndicator from "@/components/StepsIndicator";
+import { mapState, mapMutations, mapActions } from 'vuex'
+import StepsIndicator from '@/components/StepsIndicator'
 export default {
   computed: {
-    ...mapState(["name", "mobile", "address", "deliverType", "fileKey", "wxReady"]),
-    ...mapActions(["onUploadFile"]),
-    validateName: function() {
+    ...mapState([
+      'name',
+      'mobile',
+      'address',
+      'deliverType',
+      'fileKey',
+      'wxReady'
+    ]),
+    ...mapActions(['onUploadFile']),
+    validateName: function () {
       if (!this.name) {
-        return false;
+        return false
       }
-      const r = /^[\u4E00-\u9FA5]{2,4}$/;
-      return r.test(this.name);
+      const r = /^[\u4E00-\u9FA5]{2,4}$/
+      return r.test(this.name)
     },
-    validateMobile: function() {
+    validateMobile: function () {
       if (!this.mobile) {
-        return false;
+        return false
       }
-      const r = /^1\d{10}$/;
-      return r.test(this.mobile);
+      const r = /^1\d{10}$/
+      return r.test(this.mobile)
     },
-    validateAddress: function() {
-      console.log('this.address', this.address);
+    validateAddress: function () {
+      console.log('this.address', this.address)
       if (!this.address) {
-        return false;
+        return false
       }
-      return true;
+      return true
     },
-    validateNextBtn: function() {
+    validateNextBtn: function () {
       if (this.validateName && this.validateMobile && this.fileKey) {
-        if (this.deliverType === "self") {
-          return true;
+        if (this.deliverType === 'self') {
+          return true
         } else {
-          return this.validateAddress;
+          return this.validateAddress
         }
       }
-      return false;
+      return false
     }
   },
-  data: function() {
+  data: function () {
     return {
       hour: null
-    };
+    }
   },
 
   methods: {
-    getWxAddressHander() {
+    getWxAddressHander () {
       wx.openAddress({
         success: res => {
-          var userName = res.userName; // 收货人姓名
-          var postalCode = res.postalCode; // 邮编
-          var provinceName = res.provinceName; // 国标收货地址第一级地址（省）
-          var cityName = res.cityName; // 国标收货地址第二级地址（市）
-          var countryName = res.countryName; // 国标收货地址第三级地址（国家）
-          var detailInfo = res.detailInfo; // 详细收货地址信息
-          var nationalCode = res.nationalCode; // 收货地址国家码
-          var telNumber = res.telNumber; // 收货人手机号码
-          this.$store.commit("updateItem", {
-            key: "address",
+          const userName = res.userName // 收货人姓名
+          const postalCode = res.postalCode // 邮编
+          const provinceName = res.provinceName // 国标收货地址第一级地址（省）
+          const cityName = res.cityName // 国标收货地址第二级地址（市）
+          const countryName = res.countryName // 国标收货地址第三级地址（国家）
+          const detailInfo = res.detailInfo // 详细收货地址信息
+          const nationalCode = res.nationalCode // 收货地址国家码
+          const telNumber = res.telNumber // 收货人手机号码
+          this.$store.commit('updateItem', {
+            key: 'address',
             value:
               res.provinceName + res.cityName + res.countryName + res.detailInfo
-          });
+          })
+          this.$store.commit('updateItem', {
+            key: 'mobile',
+            value: telNumber
+          })
+          this.$store.commit('updateItem', {
+            key: 'name',
+            value: userName
+          })
         }
-      });
+      })
     },
-    inputBlur() {
-      window.scrollTo(0, 0);
+    inputBlur () {
+      window.scrollTo(0, 0)
     },
-    changeInputAddress(e) {
-      this.$store.commit("updateItem", {
-        key: "address",
+    changeInputAddress (e) {
+      this.$store.commit('updateItem', {
+        key: 'address',
         value: e.target.value
-      });
+      })
     },
-    changeInputName(e) {
-      this.$store.commit("updateItem", {
-        key: "name",
+    changeInputName (e) {
+      this.$store.commit('updateItem', {
+        key: 'name',
         value: e.target.value
-      });
+      })
     },
-    changeInputMobile(e) {
-      this.$store.commit("updateItem", {
-        key: "mobile",
+    changeInputMobile (e) {
+      this.$store.commit('updateItem', {
+        key: 'mobile',
         value: e.target.value
-      });
+      })
     },
-    chageStepIndexToNext() {
+    chageStepIndexToNext () {
       if (this.validateNextBtn) {
-        this.$store.commit("chageStepIndex", 3);
+        this.$store.commit('chageStepIndex', 3)
       }
     },
 
-    chageStepIndexTpPerv() {
-      this.$store.commit("chageStepIndex", 1);
+    chageStepIndexTpPerv () {
+      this.$store.commit('chageStepIndex', 1)
     },
-    changeTab(type) {
-      this.$store.commit("updateItem", {
-        key: "deliverType",
+    changeTab (type) {
+      this.$store.commit('updateItem', {
+        key: 'deliverType',
         value: type
-      });
+      })
     },
 
-    onClickTakePhoto(e) {
+    onClickTakePhoto (e) {
       if (e.target.files.length) {
-        this.$store.dispatch("onUploadFile", {
+        this.$store.dispatch('onUploadFile', {
           apolloClient: this.$apollo.provider.defaultClient,
           file: e.target.files[0]
-        });
+        })
       }
       // window.wx.chooseImage({
       //   count: 1, // 默认9
@@ -252,10 +274,13 @@ export default {
   components: {
     StepsIndicator
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
+.padding-bottom-30{
+  padding-bottom: 60px;
+}
 .pageTitles {
   font-size: 48px;
   color: #c19b25;
@@ -344,7 +369,7 @@ export default {
         justify-content: center;
         align-items: center;
         &::after {
-          content: " ";
+          content: ' ';
           display: block;
           width: 38px;
           height: 38px;
