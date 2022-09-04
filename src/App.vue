d992b8f514bbb847230b35ef2e95ac8b817fd6d8
唐云 <mr.tangyun@gmail.com>
2022-09-04 12:29:07 +0800

    修复bug

--- src/App.vue
+++ src/App.vue
@@ -48,6 +48,7 @@ export default {
       });
       window.wx.error(function(res) {
         console.log("res------", res);
+        alert('错误：' + res.errMsg);
         // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
       });
 
